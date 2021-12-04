74f05ceb7a39d73574a2f8e31ffb8975cf681e37
JanHoly <53258234+hongolem@users.noreply.github.com>
2021-12-04 13:26:44 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 Time = 0
-Guess = 0
+Guess1 = 0
+Guess2 = 0
+Result = 0
 
 def on_button_pressed_a():
     music.play_tone(262, Time)
@@ -12,8 +14,15 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
-    global Guess
-    Guess = control.millis()
-    led.plot_bar_graph(Guess, Time)
-    basic.pause(5000)
+    global Guess1
+    Guess1 = control.millis()
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def on_logo_released():
+    global Guess2, Result
+    Guess2 = control.millis()
+    Result = Guess2 - Guess1
+    led.plot_bar_graph(Result, Time)
+    basic.pause(5000)
+    basic.clear_screen()
+input.on_logo_event(TouchButtonEvent.RELEASED, on_logo_released)
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let Time = 0
-let Guess = 0
+let Guess1 = 0
+let Guess2 = 0
+let Result = 0
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     music.playTone(262, Time)
 })
@@ -10,7 +12,13 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     
-    Guess = control.millis()
-    led.plotBarGraph(Guess, Time)
+    Guess1 = control.millis()
+})
+input.onLogoEvent(TouchButtonEvent.Released, function on_logo_released() {
+    
+    Guess2 = control.millis()
+    Result = Guess2 - Guess1
+    led.plotBarGraph(Result, Time)
     basic.pause(5000)
+    basic.clearScreen()
 })
